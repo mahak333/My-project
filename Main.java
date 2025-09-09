88b0f65a3a14ffa9d133a154d5a730f6392cbef5
Mahak <patlemahak88@gmail.com>
2025-09-09 11:15:05 +0530

    V1_Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,27 @@
-
+import java.util.Scanner;
+Class Calculator{
+  public static void main(String args[]){
+    System.out.println("Enter two numbers:");
+    Scanner sc=new Scanner(System.in);
+    int num1=sc.nextInt();
+    int num2=sc.nextInt();
+    System.out.println("Enter the operator:");
+    int operator=sc.next();
+    switch(operator){
+      case '+':
+        Add(num1,num2);
+        break;
+      case '-':
+        Sub(num1,num2);
+        break;
+      case'*':
+        Mult(num1,num2);
+        break;
+      case'/':
+        Div(num1,num2);
+        break;
+      default:
+        System.out.println("Enter valid operator");
+}
+}
+}
