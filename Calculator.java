c23fd51a47ab03060c02f19130bc7e928784831b
Mahak <patlemahak88@gmail.com>
2025-09-09 11:20:18 +0530

    V1_Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,12 @@
-
+public void Add(num1,num2){
+  System.out.println("The sum of two numbers is :"+(a+b));
+}
+public void Sub(num1,num2){
+  System.out.println("The subtraction of two numbers is :"+(a-b));
+}
+public void Mult(num1,num2){
+  System.out.println("The multiplication of two numbers is :"+(a*b));
+}
+public void Div(num1,num2){
+  System.out.println("The division of two numbers is :"+(a/b));
+}
